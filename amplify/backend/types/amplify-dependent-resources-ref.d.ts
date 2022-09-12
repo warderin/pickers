2f856a33692b3832b7f4ef428dda3acce0105162
Erin Ward 🍕 <warderin@amazon.com>
2022-09-11 19:40:26 -0700

    Commit backend

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,26 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "pickers": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "auth": {
+        "pickers": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "storage": {
+        "s3pickersstorage84b10149": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    }
+}
